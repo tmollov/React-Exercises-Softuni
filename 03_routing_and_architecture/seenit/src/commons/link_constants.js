9a7e9729be4a74dd5208d50f730dffc3e670e705
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-07 00:34:08 +0300

    Update link_constants.js

--- 03_routing_and_architecture/seenit/src/commons/link_constants.js
+++ 03_routing_and_architecture/seenit/src/commons/link_constants.js
@@ -1,10 +1,14 @@
 const links = {
     home: '/',
     my_posts: '/myposts',
-    submit: "/submit",
-    post: '/posts/:id',
+    submit: '/submit',
+    post_detail: '/posts/:id',
+    edit_post: '/posts/edit/:id',
     to_post: (id) => {
         return `/posts/${id}`;
+    },
+    to_edit_post: (id) => {
+        return `/posts/edit/${id}`
     }
 }
 
