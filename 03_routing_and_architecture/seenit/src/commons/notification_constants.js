e618bdf3fd1765416d634c0696a5e8089a4994e0
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-06 18:03:55 +0300

    Renamed file

---
+++ 03_routing_and_architecture/seenit/src/commons/notification_constants.js
@@ -0,0 +1,18 @@
+const messages = {
+    loading: "Loading...",
+    logout: "Logged out!",
+    passwords_not_match: "Passwords doesn't match",
+    signed:"Signed In!",
+
+    invalid_post: "Post is invalid!",
+    uploading_post: "Uploading post...",
+    post_uploaded: "Post created."
+}
+
+const types = {
+    error: "error",
+    info: "info",
+    loading: "loading"
+}
+
+export { types, messages}
