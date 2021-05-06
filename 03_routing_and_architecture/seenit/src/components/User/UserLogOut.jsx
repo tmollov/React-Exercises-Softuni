3ac912bc16501cbe742ad88e824409db2db65536
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-06 19:11:35 +0300

    Moved logout logic from component to service

--- 03_routing_and_architecture/seenit/src/components/User/UserLogOut.jsx
+++ 03_routing_and_architecture/seenit/src/components/User/UserLogOut.jsx
@@ -3,11 +3,12 @@ import NotificationService from "../../services/notificationService";
 import {message, types, messages} from "../../commons/notification_constants";
 import {Redirect} from "react-router-dom";
 import links from "../../commons/link_constants";
+import authService from "../../services/authService";
 
 export default class UserLogOut extends Component {
     logout = () => {
+        authService.log_out()
         NotificationService.showMessage(message(types.info, messages.logout));
-        localStorage.clear();
     };
 
     render = () => {
