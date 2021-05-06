1c1a594205f53a822154577712867328dae35253
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-06 18:18:50 +0300

    Created PostForm to get data and render Edit Post component

---
+++ 03_routing_and_architecture/seenit/src/components/Post/PostEditForm.jsx
@@ -0,0 +1,66 @@
+import {input_type, input_name, labels} from '../../commons/input_constants';
+
+import React from 'react'
+import {useFormik} from "formik";
+import InputArea from "../common/InputArea";
+import TextArea from "../common/TextArea";
+import ValidationService from "../../services/validationService";
+
+const validate = ValidationService.submit_form_validation;
+
+export default function PostEditForm({post}) {
+    const f = useFormik({
+        enableReinitialize:true,
+        initialValues:post,
+        validate,
+        onSubmit: values => {
+            console.log(values);
+        }
+    });
+
+    return (
+        <div className="submitArea formContainer">
+
+            <form id="submitForm" className="submitForm" onSubmit={f.handleSubmit}>
+
+                <InputArea
+                    label={labels.link_title}
+                    name={input_name.title}
+                    type={input_type.text}
+                    change={f.handleChange}
+                    value={f.values.title}
+                    errors={f.errors.title}
+                    required={true}
+                />
+
+                <InputArea
+                    label={labels.link_url}
+                    name={input_name.url}
+                    type={input_type.url}
+                    change={f.handleChange}
+                    value={f.values.url}
+                    errors={f.errors.url}
+                    required={true}
+                />
+
+                <InputArea
+                    label={labels.link_thumbnail}
+                    name={input_name.thumbnail}
+                    type={input_type.url}
+                    change={f.handleChange}
+                    value={f.values.thumbnail}
+                    errors={f.errors.thumbnail}
+                />
+
+                <TextArea
+                    label={labels.link_comment}
+                    name={input_name.comment}
+                    change={f.handleChange}
+                    value={f.values.comment}
+                />
+
+                <input type={input_type.submit} value={labels.submit}></input>
+            </form>
+        </div>
+    );
+}
---
+++ 03_routing_and_architecture/seenit/src/components/Post/PostForm.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import PostService from "../../services/postService";
+import PostEditForm from "./PostEditForm";
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            url: "",
+            image: "",
+            comment: ""
+        };
+    }
+
+    componentDidMount() {
+        let id = this.props.match.params.id;
+        PostService.get_post(id).then((res) => {
+            this.setState(res);
+        })
+    }
+
+    showEdit = () => {
+        if (Object.values(this.state).length > 4){
+            return <PostEditForm post={this.state} {...this.props}/>
+        }
+    }
+
+    render() {
+        return (
+            <section id="viewSubmit">
+
+                <div className="submitArea">
+                    <h1>Edit Link</h1>
+                    <p>Please, fill out the form. A thumbnail image & comment is not required.</p>
+                </div>
+
+                {this.showEdit()}
+            </section>
+        );
+    }
+}
+
+export default PostForm;
