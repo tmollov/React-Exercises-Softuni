a4acc5ec1a87c7d8429e799667c8b16d5e449790
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-07 22:07:49 +0300

    Update MyPosts.jsx
    Now components is used with WithAuth HOC in App.js

--- 03_routing_and_architecture/seenit/src/components/Post/MyPosts.jsx
+++ 03_routing_and_architecture/seenit/src/components/Post/MyPosts.jsx
@@ -1,60 +1,58 @@
-import React, { Component } from 'react'
-import { routes } from '../common/RouteValidation';
+import React, {Component} from 'react'
 
 export default class MyPosts extends Component {
 
-  render() {
-    return (
-      <section id="viewMyPosts">
-        {routes.tryRedirect()}
+    render() {
+        return (
+            <section id="viewMyPosts">
 
-        <div className="post post-content">
-          <h1>Your Posts</h1>
-        </div>
-        <div className="posts">
-          <article className="post">
-            <div className="col rank">
-              <span>1</span>
-            </div>
-            <div className="col thumbnail">
-              <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
-                <img src="https://res.cloudinary.com/sucimages/image/upload/v1545672082/sample.jpg"
-                  alt="mypost" />
-              </a>
-            </div>
-            <div className="post-content">
-              <div className="title">
-                <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
-                  Sammy Docs
-              </a>
-              </div>
-              <div className="details">
-                <div className="info">submitted 5 days ago by pesho</div>
-                <div className="controls">
-                  <ul>
-                    <li className="action">
-                      <a className="commentsLink" href="/">
-                        comments
-                    </a>
-                    </li>
-                    <li className="action">
-                      <a className="editLink" href="/" >
-                        edit
-                    </a>
-                    </li>
-                    <li className="action">
-                      <a className="deleteLink" href="/">
-                        delete
-                    </a>
-                    </li>
-                  </ul>
+                <div className="post post-content">
+                    <h1>Your Posts</h1>
                 </div>
-              </div>
-            </div>
-            <div className="clear"></div>
-          </article>
-        </div>
-      </section>
-    )
-  }
+                <div className="posts">
+                    <article className="post">
+                        <div className="col rank">
+                            <span>1</span>
+                        </div>
+                        <div className="col thumbnail">
+                            <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
+                                <img src="https://res.cloudinary.com/sucimages/image/upload/v1545672082/sample.jpg"
+                                     alt="mypost"/>
+                            </a>
+                        </div>
+                        <div className="post-content">
+                            <div className="title">
+                                <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
+                                    Sammy Docs
+                                </a>
+                            </div>
+                            <div className="details">
+                                <div className="info">submitted 5 days ago by pesho</div>
+                                <div className="controls">
+                                    <ul>
+                                        <li className="action">
+                                            <a className="commentsLink" href="/">
+                                                comments
+                                            </a>
+                                        </li>
+                                        <li className="action">
+                                            <a className="editLink" href="/">
+                                                edit
+                                            </a>
+                                        </li>
+                                        <li className="action">
+                                            <a className="deleteLink" href="/">
+                                                delete
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="clear"></div>
+                    </article>
+                </div>
+            </section>
+        )
+    }
 }
