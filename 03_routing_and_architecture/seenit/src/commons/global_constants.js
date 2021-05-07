6d3deafdebe28648170df0515df602de252df5ba
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-07 20:58:09 +0300

    Update global_constants.js
    Formatted file and added new constant/s

--- 03_routing_and_architecture/seenit/src/commons/global_constants.js
+++ 03_routing_and_architecture/seenit/src/commons/global_constants.js
@@ -1,19 +1,23 @@
 const capitalized = {
     password: "Password",
-    https:"HTTPS://",
-    http:"HTTP://"
+    https: "HTTPS://",
+    http: "HTTP://"
 }
 
 const protocols = {
-    https:"https://",
-    http:"http://"
+    https: "https://",
+    http: "http://"
 }
 
 const regexes = {
-    password:/^[a-z0-9]{6,}$/i,
-    email:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+    password: /^[a-z0-9]{6,}$/i,
+    email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
     // eslint-disable-next-line
-    url:/[https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
+    url: /[https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
 }
 
-export {capitalized, regexes, protocols}
+const other = {
+    no_image: "https://gaprastore.com/wp-content/uploads/2020/12/no_image.jpg"
+}
+
+export {capitalized, regexes, protocols, other}
