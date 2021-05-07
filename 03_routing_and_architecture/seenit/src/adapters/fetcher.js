c32101a309766dc82197680dbf3ea78882ad9b41
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-07 20:57:42 +0300

    Update fetcher.js
    Added put funtion

--- 03_routing_and_architecture/seenit/src/adapters/fetcher.js
+++ 03_routing_and_architecture/seenit/src/adapters/fetcher.js
@@ -12,6 +12,11 @@ const endpoints = {
     }
 }
 
+const m = {
+    post: "POST",
+    put: "PUT"
+}
+
 const fetcher = {
     get: (endpoint, callback) =>
         fetch(BASE_URL + endpoint)
@@ -23,7 +28,7 @@ const fetcher = {
 
     post: (endpoint, data, callback) => {
         let request = {
-            method: 'POST',
+            method: m.post,
             //mode: 'cors', // no-cors, *cors, same-origin
             headers: {
                 'Content-Type': 'application/json'
@@ -36,7 +41,24 @@ const fetcher = {
             .catch((err) => {
                 return err
             });
-    }
+    },
+
+    put: (endpoint, data, callback) => {
+        let request = {
+            method: m.put,
+            //mode: 'cors', // no-cors, *cors, same-origin
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }
+        return fetch(BASE_URL + endpoint, request, callback)
+            .then(data => data.json())
+            .then(callback)
+            .catch((err) => {
+                return err
+            });
+    },
 }
 
 export {fetcher, endpoints};
