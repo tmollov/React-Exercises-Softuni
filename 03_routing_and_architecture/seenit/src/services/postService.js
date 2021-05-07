fc863faf1ec9bf232273be601b354c2f6ccacbe6
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-05-07 22:03:27 +0300

    Update postService.js
    Just arranged functions for better readability

--- 03_routing_and_architecture/seenit/src/services/postService.js
+++ 03_routing_and_architecture/seenit/src/services/postService.js
@@ -3,19 +3,45 @@ import notificationService from "./notificationService";
 import {messages, types} from "../commons/notification_constants";
 import AuthState from "../adapters/authState";
 import {endpoints, fetcher} from "../adapters/fetcher";
+import validators from "../commons/validation_constants";
+
+const f = {
+    trim_post: function (post) {
+        post.title = validators.escapeHtml(post.title.trim());
+        post.url = validators.escapeHtml(post.url.trim());
+        post.thumbnail = validators.escapeHtml(post.thumbnail.trim());
+        post.comment = validators.escapeHtml(post.comment.trim());
+
+        return post;
+    }
+}
 
 const PostService = {
-    add_post(post) {
-        console.log(post)
-        if (!ValidationService.validate_post(post)) {
-            notificationService.showMessage(types.error,messages.invalid_post);
-        }
-        notificationService.showMessage(types.loading,messages.uploading_post)
 
-        post.title = post.title.trim();
-        post.url = post.url.trim();
-        post.thumbnail = post.thumbnail.trim();
-        post.comment = post.comment.trim();
+    get_posts() {
+        return fetcher.get(endpoints.posts, (res) => {
+            //notificationService.showMessage(types.info, messages.post_uploaded)
+            return res;
+        })
+    },
+    get_post(id) {
+        return fetcher.get(endpoints.post(id), (res) => {
+            return res;
+        })
+    },
+    get_post_for_edit(id) {
+        return fetcher.get(endpoints.edit_post(id), (res) => {
+            return res;
+        })
+    },
+
+    add_post(post) {
+        if (!ValidationService.validate_post(post)) {
+            notificationService.showMessage(types.error, messages.invalid_post);
+        }
+        notificationService.showMessage(types.loading, messages.uploading_post)
+
+        post = f.trim_post(post);
 
         post.author = AuthState.auth.username;
         post.date = new Date();
@@ -25,15 +51,15 @@ const PostService = {
             return res.id;
         })
     },
-    get_posts() {
-        return fetcher.get(endpoints.posts, (res) => {
-            //notificationService.showMessage(types.info, messages.post_uploaded)
-            return res;
-        })
-    },
-    get_post(id) {
-        return fetcher.get(endpoints.post(id), (res) => {
-            //notificationService.showMessage(types.info, messages.post_uploaded)
+    update_post(post) {
+        if (!ValidationService.validate_post(post)) {
+            notificationService.showMessage(types.error, messages.invalid_post);
+        }
+        notificationService.showMessage(types.loading, messages.post_updated)
+
+        post = f.trim_post(post);
+
+        return fetcher.put(endpoints.post(post.id), post, (res) => {
             return res;
         })
     }
