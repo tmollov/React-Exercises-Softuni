6fc1f6a53004c7cc945f87dbf9ba7be129c8b79a
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-06-01 19:54:38 +0300

    Update withAuth.js
    Using authState for re-rendering

--- 03_routing_and_architecture/seenit/src/hocs/withAuth.js
+++ 03_routing_and_architecture/seenit/src/hocs/withAuth.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import authService from "../services/authService";
 import {Redirect} from "react-router-dom";
+import AuthState from "../adapters/authState";
 
 function WithAuth(WrappedComponent) {
     return class extends React.Component {
         showContent = () => {
-            if (authService.isUserLogged) {
+            if (AuthState.auth.jwt !== null) {
                 return <WrappedComponent {...this.props} />;
             } else {
                 return <Redirect to={"/"}/>
