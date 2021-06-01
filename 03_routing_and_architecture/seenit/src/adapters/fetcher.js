3d2392d909060d0a38e70bbb0b625f72b5369f11
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-06-01 18:30:09 +0300

    Update fetcher.js
    added 2 new methods
    added delete prop

--- 03_routing_and_architecture/seenit/src/adapters/fetcher.js
+++ 03_routing_and_architecture/seenit/src/adapters/fetcher.js
@@ -4,17 +4,31 @@ const endpoints = {
     login: "/login",
     register: "/register",
     posts: "/posts",
+    comments: "/comments",
+    my_posts: function (author) {
+        return `${this.posts}?author=${author}`;
+    },
     post: function (id) {
         return `${this.posts}/${id}`
     },
     edit_post: function (id) {
         return `${this.posts}/edit/${id}`
+    },
+    delete_post: function (id) {
+        return this.post(id);
+    },
+    comments_for_post: function (post_id) {
+        return `${this.comments}?post_id=${post_id}`;
+    },
+    comment: function (id) {
+        return `${this.comments}/${id}`
     }
 }
 
 const m = {
     post: "POST",
-    put: "PUT"
+    put: "PUT",
+    delete: "DELETE"
 }
 
 const fetcher = {
@@ -51,6 +65,21 @@ const fetcher = {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }
+        return fetch(BASE_URL + endpoint, request, callback)
+            .then(data => data.json())
+            .then(callback)
+            .catch((err) => {
+                return err
+            });
+    },
+    delete: (endpoint, callback) => {
+        let request = {
+            method: m.delete,
+            //mode: 'cors', // no-cors, *cors, same-origin
+            headers: {
+                'Content-Type': 'application/json'
+            },
         }
         return fetch(BASE_URL + endpoint, request, callback)
             .then(data => data.json())
