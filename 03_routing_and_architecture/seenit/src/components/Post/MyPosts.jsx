3be97f0dce3afe4f74ab2efe1ed7d069b7b817f7
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-06-01 19:16:17 +0300

    Update MyPosts.jsx
    removed static code

--- 03_routing_and_architecture/seenit/src/components/Post/MyPosts.jsx
+++ 03_routing_and_architecture/seenit/src/components/Post/MyPosts.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PostsList from "./Catalog/PostsList";
 
 export default class MyPosts extends Component {
 
@@ -9,49 +10,7 @@ export default class MyPosts extends Component {
                 <div className="post post-content">
                     <h1>Your Posts</h1>
                 </div>
-                <div className="posts">
-                    <article className="post">
-                        <div className="col rank">
-                            <span>1</span>
-                        </div>
-                        <div className="col thumbnail">
-                            <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
-                                <img src="https://res.cloudinary.com/sucimages/image/upload/v1545672082/sample.jpg"
-                                     alt="mypost"/>
-                            </a>
-                        </div>
-                        <div className="post-content">
-                            <div className="title">
-                                <a href="http://sammyjs.org/docs/api/0.7.4/all#Sammy.RenderContext-load">
-                                    Sammy Docs
-                                </a>
-                            </div>
-                            <div className="details">
-                                <div className="info">submitted 5 days ago by pesho</div>
-                                <div className="controls">
-                                    <ul>
-                                        <li className="action">
-                                            <a className="commentsLink" href="/">
-                                                comments
-                                            </a>
-                                        </li>
-                                        <li className="action">
-                                            <a className="editLink" href="/">
-                                                edit
-                                            </a>
-                                        </li>
-                                        <li className="action">
-                                            <a className="deleteLink" href="/">
-                                                delete
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="clear"></div>
-                    </article>
-                </div>
+                <PostsList myposts={true}/>
             </section>
         )
     }
