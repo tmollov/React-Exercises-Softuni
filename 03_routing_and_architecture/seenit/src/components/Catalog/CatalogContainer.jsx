7934a5104483f9c8acd115f4b291d364986edc86
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-06-01 18:32:38 +0300

    Update CatalogContainer.jsx
    formatted code

--- 03_routing_and_architecture/seenit/src/components/Catalog/CatalogContainer.jsx
+++ 03_routing_and_architecture/seenit/src/components/Catalog/CatalogContainer.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PostsList from '../Post/Catalog/PostsList'
 
 export default class CatalogContainer extends Component {
-  render() {
-    return (
-      <section id="viewCatalog">
-        <PostsList />
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section id="viewCatalog">
+                <PostsList/>
+            </section>
+        );
+    }
 }
