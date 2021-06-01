0980d0803689f8a416e23eb66a04bafcf688bf08
Tundzhay Mollov <tmollov@users.noreply.github.com>
2021-06-01 18:32:16 +0300

    Update notification_constants.js
    new constants

--- 03_routing_and_architecture/seenit/src/commons/notification_constants.js
+++ 03_routing_and_architecture/seenit/src/commons/notification_constants.js
@@ -7,7 +7,10 @@ const messages = {
     invalid_post: "Post is invalid!",
     uploading_post: "Uploading post...",
     post_uploaded: "Post created.",
-    post_updated: "Post updated."
+    post_updated: "Post updated.",
+    post_removed: "Post removed.",
+    comment_posted: "Comment posted.",
+    comment_deleted: "Comment deleted."
 }
 
 const types = {
